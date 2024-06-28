08e99e89690c02a1917ddc8fa917cfd81c59cc25
Mariklimova <mariku22@yandex.ru>
2024-06-29 00:22:35 +0300

    work with App page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import style from './app.module.css'
 
 function App() {
-  
+
 
   return (
     <>
-      <h1>hello</h1>
+      <div className={style.container}>
+        <div className={style.current_info}>
+          <div className={style.data_container}>
+            <div className={style.time}>
+              12:30 <span>PM</span>
+            </div>
+            <div className={style.date}>
+              Monday, 24 May
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
