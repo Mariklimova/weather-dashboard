7e441ceb3f5f0323fbcff2325de56d259354390b
Mariklimova <mariku22@yandex.ru>
2024-08-03 00:11:34 +0300

    added checks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import style from './app.module.css'
+// import style from './app.module.css'
 import SearchForm from './components/SearchForm/SearchForm';
 import WeatherDisplay from './components/WeatherDisplay/WeatherDisplay';
 
@@ -8,14 +8,23 @@ import WeatherDisplay from './components/WeatherDisplay/WeatherDisplay';
 const keyApi = '282c885c813671f84ff97415ce72cc05'
 
 function App() {
-  const [location, setLocation] = useState(localStorage.getItem('nameCity')||'');
-  const [weather, setWeather] = useState({})
+  const [location, setLocation] = useState(localStorage.getItem('nameCity') || '');
+  const [weather, setWeather] = useState({});
+  const [checkData, setCheckData] = useState(true);
+
 
 
   const getData = async () => {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${keyApi}`)
-    localStorage.setItem('nameCity', location)
-    setWeather(response.data);
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${keyApi}`);
+      localStorage.setItem('nameCity', location)
+      setWeather(response.data);
+      setCheckData(true);
+
+    } catch (error) {
+      setCheckData(false);
+
+    }
   }
 
   useEffect(() => {
@@ -25,8 +34,9 @@ function App() {
 
   return (
     <>
-      <SearchForm setLocation={setLocation} setWeather={setWeather} />
-      <WeatherDisplay weather={weather} />
+      <SearchForm setLocation={setLocation} setWeather={setWeather} checkData={checkData} setCheckData={setCheckData} />
+
+      {checkData ? <WeatherDisplay weather={weather} /> : null}
     </>
   )
 }
--- client/src/components/SearchForm/SearchForm.jsx
+++ client/src/components/SearchForm/SearchForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import style from './search.module.css'
 
-function SearchForm({ setLocation, setWeather }) {
+function SearchForm({ setLocation, setWeather, checkData, setCheckData }) {
     const [city, setCity] = useState('');
 
 
@@ -9,13 +9,16 @@ function SearchForm({ setLocation, setWeather }) {
         <div className={style.search}>
 
             <button onClick={() => {
-                setWeather({})
-                setCity('')
+                setWeather({});
+                setCity('');
+                setCheckData(true)
             }}>Reset</button>
 
             <input value={city} type="text" onChange={(e) => setCity(e.target.value)} placeholder='enter location' />
             <button onClick={() => setLocation(city)}>Search</button>
+
         </div>
+        <div> {!checkData ? <p className={style.error}>I can't determine the location, try again</p> : null}</div>
     </>
 }
 export default SearchForm
